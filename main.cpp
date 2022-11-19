1975c4d741e25d64447c21643decec1c99eb8317
Martin Nikolov <martooo3000@gmail.com>
2022-11-19 12:41:19 +0200

    problem finished: -whole code

--- main.cpp
+++ main.cpp
@@ -1,9 +1,68 @@
 #include <iostream>
 using namespace std;
 
+bool varA_in_varB(int varA, int varB)
+{
+	if (varA < 0)
+	{
+		varA *= -1;
+	}
+	if (varB < 0)
+	{
+		varB *= -1;
+	}
+
+	int tempA = varA;
+	int tempB = varB;
+	bool aInB = true;
+	bool tempCheck = false;
+	do
+	{
+		if (tempA % 10 == tempB % 10)
+		{
+			tempCheck = true;
+		}
+		if (tempB < 10)
+		{
+			tempB = varB;
+			tempA /= 10;
+			if (tempCheck == false)
+			{
+				aInB = false;
+				break;
+			}
+		}
+		else
+		{
+			tempB /= 10;
+		}
+	} while (tempA != 0);
+	return aInB;
+}
+
 int main()
 {
+	int firstNum, secondNum;
+	cin >> firstNum >> secondNum;
+	bool firstInSecond = varA_in_varB(firstNum, secondNum);
+	bool secondInFirst = varA_in_varB(secondNum, firstNum);
 
-
+	if (firstInSecond == true && secondInFirst == true)
+	{
+		cout << "Equal strength";
+	}
+	if (firstInSecond == true && secondInFirst == false)
+	{
+		cout << "First is weaker";
+	}
+	if (firstInSecond == false && secondInFirst == true)
+	{
+		cout << "Second is weaker";
+	}
+	if (firstInSecond == false && secondInFirst == false)
+	{
+		cout << "Not comparable";
+	}
+	
 	return 0;
 }
